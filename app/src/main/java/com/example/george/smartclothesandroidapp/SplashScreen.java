601cece02fd1e3f11b67db8b4b507e43d94a327a
George91 <george.siegle@gmail.com>
2015-03-28 14:02:27 -0500

    Added splash screen

---
+++ app/src/main/java/com/example/george/smartclothesandroidapp/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.george.smartclothesandroidapp;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+/**
+ * Created by George on 3/24/2015.
+ */
+public class SplashScreen extends Activity{
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent splashScreenIntent = new Intent(SplashScreen.this, MainInterface.class);
+
+                startActivity(splashScreenIntent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
